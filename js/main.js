2dbc9b1776db73008eeb21149d8b8bb80f0fc8b9
Overy <u9835050@gmail.com>
2022-11-25 23:36:01 +0800

    update start page for Scrum Biginner ver.1.0 updated date:2022-11-25

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+$(window).on('load', function () {
+    setTimeout(function() { 
+        document.getElementById("loading").style.display = "none"
+        // document.getElementById("start").style.display = "block"
+        document.getElementById("start").classList.add("active");
+      }, 1800);
+});
 $(document).ready(function() {
-
+    var startRunning = function (){
+        $(".page-label, .po, .role-user").addClass("animate__animated");
+        $( "#start-btn" ).click(function() {
+            $("#start").removeClass("active");
+            $("#story").addClass("active");
+          });
+    }
+    
+    startRunning()
 });
